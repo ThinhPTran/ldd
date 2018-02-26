e1f517bb6d0c08ebb89ff222f5dd7a757c186356
datnt <good.engineer810@gmail.com>
2018-02-27 00:50:18 +0700

    use strace to watch system calls

---
+++ phan_3/bai_3_5/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+ 
+int8_t read_buf[1024];
+int8_t write_buf[1024]="viet nam hung cuong\n";
+int main()
+{
+        int fd;
+ 
+        fd = open("/dev/char_device", O_RDWR);
+        if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return 0;
+        }
+ 
+	write(fd, write_buf, strlen(write_buf)+1);
+	read(fd, read_buf, 1024);
+	printf("Data = %s", read_buf);
+	close(fd);
+}
