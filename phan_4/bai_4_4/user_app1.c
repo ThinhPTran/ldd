1cd4d4ea724d39496504f2b7f4af706ce38fa8a7
tiendatnt <good.engineer810@gmail.com>
2018-03-29 23:59:38 +0700

    add mutex tutorial

---
+++ phan_4/bai_4_4/user_app1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+ 
+int8_t read_buf[1024];
+int8_t write_buf[1024]="application 1 hello world\n";
+int main()
+{
+        int fd;
+ 
+        fd = open("/dev/char_device", O_RDWR);
+        if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return 0;
+        }
+ 
+	write(fd, write_buf, strlen(write_buf)+1);
+	sleep(5);
+	read(fd, read_buf, 1024);
+	printf("tien trinh 1 doc tu kernel buffer cua char_driver: %s", read_buf);
+	close(fd);
+}
