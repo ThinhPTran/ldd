c6576e39c8ff352d6fa6b90c76f46f430909e640
tiendatnt <good.engineer810@gmail.com>
2018-05-06 19:32:07 +0700

    add: bai_5_1

--- phan_5/bai_5_1/char_driver_4.c
+++ phan_5/bai_5_1/char_driver_4.c
@@ -8,28 +8,42 @@
 #include<linux/cdev.h> /* thu vien cho cau truc cdev */
 #include<linux/slab.h> /* thu vien chua ham kmalloc */
 #include<linux/uaccess.h> /* thu vien chua cac ham trao doi du lieu giua user va kernel */
+#include <linux/ioctl.h>
+
+typedef struct {
+	int rd;
+	int wr;
+} status_t;
 
 #define DRIVER_AUTHOR "Nguyen Tien Dat <dat.a3cbq91@gmail.com>"
 #define DRIVER_DESC   "A simple example about character driver"
 #define MEM_SIZE 1024
+#define ENABLE 1
+#define DISABLE 0
+#define RESET_STATISTIC _IO(200, 0)
+#define WR_CONTROL_DEV _IOW(200, 1, int32_t *)
+#define RD_STATUS_DEV _IOR(200, 2, status_t *)
 
 dev_t dev_num = 0;
 static struct class * device_class;
 static struct cdev *example_cdev;
 uint8_t *kernel_buffer;
 unsigned open_cnt = 0;
+unsigned control = ENABLE;
 
 static int example_open(struct inode *inode, struct file *filp);
 static int example_release(struct inode *inode, struct file *filp);
 static ssize_t example_read(struct file *filp, char __user *user_buf, size_t len, loff_t * off);
 static ssize_t example_write(struct file *filp, const char *user_buf, size_t len, loff_t * off);
- 
+static long example_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
 static struct file_operations fops =
 {
 	.owner          = THIS_MODULE,
 	.read           = example_read,
 	.write          = example_write,
 	.open           = example_open,
+	.unlocked_ioctl = example_ioctl,
 	.release        = example_release,
 };
  
@@ -48,18 +62,48 @@ static int example_release(struct inode *inode, struct file *filp)
  
 static ssize_t example_read(struct file *filp, char __user *user_buf, size_t len, loff_t *off)
 {
+	if (control == DISABLE) return 0;
+
 	copy_to_user(user_buf, kernel_buffer, MEM_SIZE);
 	printk("Handle read event %u times\n", open_cnt);
 	return MEM_SIZE;
 }
 static ssize_t example_write(struct file *filp, const char __user *user_buf, size_t len, loff_t *off)
 {
+	if (control == DISABLE) return 0;
+
 	copy_from_user(kernel_buffer, user_buf, len);
 	printk("Handle write event %u times\n", open_cnt);
 	return len;
 }
  
-
+static long example_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	switch(cmd) {
+		case RESET_STATISTIC:
+			open_cnt = 0;
+			printk("reset open_cnt = %d\n", open_cnt);
+			break;
+		case WR_CONTROL_DEV:
+			copy_from_user(&control, (int32_t*) arg, sizeof(control));
+			printk("%s device\n", (control == DISABLE)?"disable":"enable");
+			break;
+		case RD_STATUS_DEV:
+		{
+			status_t status;
+			if(control == DISABLE) {
+				status.rd = 0;
+				status.wr = 0;
+			} else {
+				status.rd = 1;
+				status.wr = 1;
+			}
+			copy_to_user((status_t*)arg, &status, sizeof(status));
+		}
+			break;
+	}
+	return 0;
+}
 static int __init char_driver_init(void)
 {
 	/* cap phat dong device number */
--- phan_5/bai_5_1/test.c
+++ phan_5/bai_5_1/test.c
@@ -5,53 +5,81 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
- 
+#include<sys/ioctl.h>
+
+typedef struct {
+        int rd;
+        int wr;
+} status_t;
+
+#define ENABLE 1
+#define DISABLE 0
+#define RESET_STATISTIC _IO(200, 0)
+#define WR_CONTROL_DEV _IOW(200, 1, int32_t *)
+#define RD_STATUS_DEV _IOR(200, 2, status_t *)
+
 int8_t write_buf[1024];
 int8_t read_buf[1024];
-int main()
-{
-        int fd;
-        char option;
-        printf("**************************************************\n");
-        printf("*******user application to test char driver*******\n");
- 
-        fd = open("/dev/char_device", O_RDWR);
+int transfer_data() {
+        int fd = open("/dev/char_device", O_RDWR);
         if(fd < 0) {
                 printf("Cannot open device file...\n");
                 return 0;
         }
- 
-        while(1) {
-                printf("****Please Enter the Option******\n");
-                printf("        1. Write               \n");
-                printf("        2. Read                 \n");
-                printf("        3. Exit                 \n");
-                printf("*********************************\n");
-                scanf(" %c", &option);
-                printf("Your Option = %c\n", option);
-                
-                switch(option) {
-                        case '1':
-                                printf("Enter the string to write into driver :");
-                                scanf("  %[^\t\n]s", write_buf);
-                                printf("Data Writing ...");
-                                write(fd, write_buf, strlen(write_buf)+1);
-                                printf("Done!\n");
-                                break;
-                        case '2':
-                                printf("Data Reading ...");
-                                read(fd, read_buf, 1024);
-                                printf("Done!\n\n");
-                                printf("Data = %s\n\n", read_buf);
-                                break;
-                        case '3':
-                                close(fd);
-                                exit(1);
-                                break;
-                        default:
-                                printf("Enter Valid option = %c\n",option);
-                                break;
-                }
-        }
+
+        printf("Enter the string to write into driver :");
+        scanf("  %[^\t\n]s", write_buf);
+        if(write(fd, write_buf, strlen(write_buf)+1) == 0) return -1;
+        printf("Data Reading ...");
+        if(read(fd, read_buf, 1024) == 0) return -1;
+        printf("Data = %s\n\n", read_buf);
         close(fd);
+	return 0;
+}
+
+void control_char_device(int32_t control) {
+	int fd = open("/dev/char_device", O_RDWR);
+	if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return;
+        }
+	ioctl(fd, WR_CONTROL_DEV, (int32_t*) &control);
+
+	close(fd);
+}
+
+void read_status_char_device() {
+	status_t status;
+	int fd = open("/dev/char_device", O_RDWR);
+        if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return;
+        }
+        ioctl(fd, RD_STATUS_DEV, (status_t*) &status);
+	printf("status read (%d), write(%d)\n", status.rd, status.wr);
+
+        close(fd);
+}
+
+void reset_statistic() {
+        int fd = open("/dev/char_device", O_RDWR);
+        if(fd < 0) {
+                printf("Cannot open device file...\n");
+                return;
+        }
+        ioctl(fd, RESET_STATISTIC);
+
+        close(fd);
+}
+
+int main()
+{
+	control_char_device(DISABLE);//the command will make disable data transfering
+	if(transfer_data() < 0) printf("no data transfering\n");//expect no data transfer
+	read_status_char_device();
+	control_char_device(ENABLE);//the command will make enable data transfering
+	if(transfer_data() < 0) printf("no data transfering\n");//expect have data transfer
+	read_status_char_device();
+	reset_statistic();
+	return 0;
 }
