ebbc3cabb7b827a0fbb59055d2d0c4e9c19fff5c
tiendatnt <good.engineer810@gmail.com>
2018-06-24 12:51:21 +0700

    add code top-half before add code bottom-half

--- phan_8/bai_8_4/char_driver_4.c
+++ phan_8/bai_8_4/char_driver_4.c
@@ -8,16 +8,19 @@
 #include<linux/cdev.h> /* thu vien cho cau truc cdev */
 #include<linux/slab.h> /* thu vien chua ham kmalloc */
 #include<linux/uaccess.h> /* thu vien chua cac ham trao doi du lieu giua user va kernel */
+#include<linux/interrupt.h> /* thu vien chua cac ham dang ky va dieu khien ngat */
 
 #define DRIVER_AUTHOR "Nguyen Tien Dat <dat.a3cbq91@gmail.com>"
 #define DRIVER_DESC   "A simple example about character driver"
 #define MEM_SIZE 1024
+#define IRQ_NUMBER 9
 
 dev_t dev_num = 0;
 static struct class * device_class;
 static struct cdev *example_cdev;
 uint8_t *kernel_buffer;
 unsigned open_cnt = 0;
+volatile uint32_t int_cnt = 0;
 
 static int example_open(struct inode *inode, struct file *filp);
 static int example_release(struct inode *inode, struct file *filp);
@@ -50,15 +53,25 @@ static ssize_t example_read(struct file *filp, char __user *user_buf, size_t len
 {
 	copy_to_user(user_buf, kernel_buffer, MEM_SIZE);
 	printk("Handle read event %u times\n", open_cnt);
+	printk("interrupt counter = %u\n", int_cnt);
 	return MEM_SIZE;
 }
 static ssize_t example_write(struct file *filp, const char __user *user_buf, size_t len, loff_t *off)
 {
 	copy_from_user(kernel_buffer, user_buf, len);
 	printk("Handle write event %u times\n", open_cnt);
+
+	/* gia lap ngat tren IRQ9 */
+	asm("int $0x39");
+
 	return len;
 }
  
+static irqreturn_t top_half_isr(int irq, void *dev)
+{
+	int_cnt++;
+	return IRQ_HANDLED;
+}
 
 static int __init char_driver_init(void)
 {
@@ -78,11 +91,18 @@ static int __init char_driver_init(void)
 	cdev_init(example_cdev, &fops);
 	cdev_add(example_cdev, dev_num, 1);
 
+	/* dang ky trinh phuc vu ngat */
+	if(request_irq(IRQ_NUMBER, top_half_isr, IRQF_SHARED, "char_device", (void*)(example_cdev))) {
+		printk("error Register ISR\n");
+		return -EIO;
+	}
+
 	return 0;
 }
 
 void __exit char_driver_exit(void)
 {
+	free_irq(IRQ_NUMBER, &example_cdev);
 	cdev_del(example_cdev);
 	kfree(kernel_buffer);
 	device_destroy(device_class,dev_num);
