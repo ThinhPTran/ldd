a98e9328df362dbb4f1cccda36083c28b26cf7ed
tiendatnt <good.engineer810@gmail.com>
2018-09-23 23:14:25 +0700

    Add: demo adding a file in sysfs

--- phan_10/bai_10_2/char_driver_4.c
+++ phan_10/bai_10_2/char_driver_4.c
@@ -8,6 +8,7 @@
 #include<linux/cdev.h> /* thu vien cho cau truc cdev */
 #include<linux/slab.h> /* thu vien chua ham kmalloc */
 #include<linux/uaccess.h> /* thu vien chua cac ham trao doi du lieu giua user va kernel */
+#include<linux/sysfs.h> /* thu vien chua cac ham de tao thu muc hoac file trong sysfs */
 
 #define DRIVER_AUTHOR "Nguyen Tien Dat <dat.a3cbq91@gmail.com>"
 #define DRIVER_DESC   "A simple example about character driver"
@@ -18,12 +19,31 @@ static struct class * device_class;
 static struct cdev *example_cdev;
 uint8_t *kernel_buffer;
 unsigned open_cnt = 0;
+struct kobject *kobj_ex;
+volatile int ex_value = 0; //bien nay bi thay doi gia tri bat thuong do userspace ghi vao
 
 static int example_open(struct inode *inode, struct file *filp);
 static int example_release(struct inode *inode, struct file *filp);
 static ssize_t example_read(struct file *filp, char __user *user_buf, size_t len, loff_t * off);
 static ssize_t example_write(struct file *filp, const char *user_buf, size_t len, loff_t * off);
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf);
+static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count);
+
+struct kobj_attribute ex_attr = __ATTR(ex_value, 0660, sysfs_show, sysfs_store);
+
+static ssize_t sysfs_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+        printk("Read data from ex_sysfs file\n");
+        return sprintf(buf, "%d", ex_value);
+}
  
+static ssize_t sysfs_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+        printk("Write data to ex_sysfs file \n");
+        sscanf(buf,"%d",&ex_value);
+        return count;
+}
+
 static struct file_operations fops =
 {
 	.owner          = THIS_MODULE,
@@ -32,7 +52,7 @@ static struct file_operations fops =
 	.open           = example_open,
 	.release        = example_release,
 };
- 
+
 static int example_open(struct inode *inode, struct file *filp)
 {
 	open_cnt++;
@@ -78,11 +98,18 @@ static int __init char_driver_init(void)
 	cdev_init(example_cdev, &fops);
 	cdev_add(example_cdev, dev_num, 1);
 
+	/* tao mot kobject va thu muc tuong ung cua no trong sysfs */
+	kobj_ex = kobject_create_and_add("ex_sysfs",kernel_kobj);
+	/* tao mot file trong thu muc tuong ung voi kobject */
+	sysfs_create_file(kobj_ex, &ex_attr.attr);
+
 	return 0;
 }
 
 void __exit char_driver_exit(void)
 {
+	kobject_put(kobj_ex);
+	sysfs_remove_file(kernel_kobj, &ex_attr.attr);
 	cdev_del(example_cdev);
 	kfree(kernel_buffer);
 	device_destroy(device_class,dev_num);
